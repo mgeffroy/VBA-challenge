0f6c3d0a5729a379138b3d903f2a01992b3b0df5
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-03-22 13:08:42 -0600

    Headers in spreadsheet

--- Stock market analysis.vb
+++ Stock market analysis.vb
@@ -1,29 +1,51 @@
 Sub Stockmarket():
-' Things needed in the homework 
+' Things needed in the homework
         'The ticker symbol.
         'Yearly change from opening price to closing price at the end of that year.
         'The percent change from opening price to the closing price at the end of that year.
-        'The total stock volume of the stock. 
+        'The total stock volume of the stock.
 
 'Things have to be valid for all spreadsheets
 
-'Plan of action 
-'Loop for all spreadsheets 
+'Plan of action
+
+'Variables: name, type,value (depending on variable)
+Dim ticker As String
+
+Dim Yearlychange As Double
+Yearlychange = 0
+
+Dim Opening_val As Double
+
+Dim Closing_val As Double
+
+Dim Percentchange As Double
+
+Dim Totstockvol As Long
+Totstockvol = 0
 
 
+'Summary table to keep track of tickers
+Dim Summary_Table_Row As Integer
+  Summary_Table_Row = 2
+  
 
-'Variables: name, type and value 
-Dim ticker as string 
-Dim Opening_price as double 
-Opening_price = 0 
+'Loop for all spreadsheets
 
-Dim Closing_price as double 
-Dim Percent_opening as double 
-DIm Percent_closing as double 
-Dim Tot_stockvol as long 
+For Each ws In Worksheets
 
-'Create new columns in spreadsheet 
+'Name headers in spreadsheet ( with a little bit of formatting)
 
+ws.Cells(1, 9).Value = "Ticker"
+ws.Cells(1, 10).Value = "Yearly Change"
+ws.Cells(1, 11).Value = "Percent Change"
+ws.Cells(1, 12).Value = "Total Stock Volume"
+ws.Range("A1:L1").Columns.AutoFit
+ws.Range("A1:L1").HorizontalAlignment = xlCenter
+
+Next ws
+
+End Sub
 
 End Sub
 Sub bonus(): 
