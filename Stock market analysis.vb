27743678fa9b7a3d05f15fbbf9b7ac602d3dd7c6
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-03-22 11:52:13 -0600

    Added variables and values

---
+++ Stock market analysis.vb
@@ -0,0 +1,37 @@
+Sub Stockmarket():
+' Things needed in the homework 
+        'The ticker symbol.
+        'Yearly change from opening price to closing price at the end of that year.
+        'The percent change from opening price to the closing price at the end of that year.
+        'The total stock volume of the stock. 
+
+'Things have to be valid for all spreadsheets
+
+'Plan of action 
+'Loop for all spreadsheets 
+
+
+
+'Variables: name, type and value 
+Dim ticker as string 
+Dim Opening_price as double 
+Opening_price = 0 
+
+Dim Closing_price as double 
+Dim Percent_opening as double 
+DIm Percent_closing as double 
+Dim Tot_stockvol as long 
+
+'Create new columns in spreadsheet 
+
+
+End Sub
+Sub bonus(): 
+
+'Make it work in all spreadsheets
+
+
+'Create variables 
+Dim Grt_increase as long 
+Dim Grt_decrease as long 
+Dim Grt_totvol as long 
