c4f15b9e13d5c388ddb9a7b67ee0e0b96b8fab44
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-03-22 17:17:25 -0600

    Added simple loop for tickers and total volume

--- Stock market analysis.vb
+++ Stock market analysis.vb
@@ -9,20 +9,15 @@ Sub Stockmarket():
 
 'Plan of action
 
+
 'Variables: name, type,value (depending on variable)
 Dim ticker As String
+Dim Totstockvol As LongLong 
 
-Dim Yearlychange As Double
-Yearlychange = 0
+'Give starting values 
 
-Dim Opening_val As Double
+Totstockvol = 0 
 
-Dim Closing_val As Double
-
-Dim Percentchange As Double
-
-Dim Totstockvol As Long
-Totstockvol = 0
 
 
 'Summary table to keep track of tickers
@@ -32,22 +27,52 @@ Dim Summary_Table_Row As Integer
 
 'Loop for all spreadsheets
 
-For Each ws In Worksheets
 
-'Name headers in spreadsheet ( with a little bit of formatting)
+For Each WS In Worksheets
 
-ws.Cells(1, 9).Value = "Ticker"
-ws.Cells(1, 10).Value = "Yearly Change"
-ws.Cells(1, 11).Value = "Percent Change"
-ws.Cells(1, 12).Value = "Total Stock Volume"
-ws.Range("A1:L1").Columns.AutoFit
-ws.Range("A1:L1").HorizontalAlignment = xlCenter
+'Name headers in sheet ( with a little bit of formatting)
 
-Next ws
+WS.Cells(1, 9).Value = "Ticker"
+WS.Cells(1, 10).Value = "Yearly Change"
+WS.Cells(1, 11).Value = "Percent Change"
+WS.Cells(1, 12).Value = "Total Stock Volume"
+WS.Range("A1:L1").Columns.AutoFit
+WS.Columns("G", "B").Columns.AutoFit
+WS.Range("A1:L1").HorizontalAlignment = xlCenter
+
+'Create last row variable
+lastrow = WS.Cells(Rows.Count, 1).End(xlUp).Row
+'Make loop
+For i = 2 To lastrow
+
+'Conditional to find different tickers 
+If WS.Cells(i,1).Value <> WS.Cells(i+1, 1).Value Then 
+
+'locating values 
+ticker = WS.Cells(i,1).Value 
+Totstockvol = Totstockvol + WS.Cells(i,7).Value
+
+'Print values 
+WS.Range("I" And Summary_Table_Row).Value = ticker 
+WS.Range("L" And Summary_Table_Row).Value = Totstockvol
+
+Summary_Table_Row = Summary_Table_Row + 1
+
+'Reset for next loop and ticker
+Totstockvol = 0 
+
+Else 
+Totstockvol = Totstockvol + WS.Cells(i,7).Value 
+
+End if 
+
+Next i
+
+Next WS
 
 End Sub
 
-End Sub
+
 Sub bonus(): 
 
 'Make it work in all spreadsheets
