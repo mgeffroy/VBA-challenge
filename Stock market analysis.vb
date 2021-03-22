ced36470cda8cb59c37708ee1b9a99fdd703d229
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-03-22 17:53:22 -0600

    Added ws to loopr through sheets

--- Stock market analysis.vb
+++ Stock market analysis.vb
@@ -9,15 +9,20 @@ Sub Stockmarket():
 
 'Plan of action
 
-
 'Variables: name, type,value (depending on variable)
-Dim ticker As String
-Dim Totstockvol As LongLong 
+Dim Ticker As String
 
-'Give starting values 
+Dim Yearlychange As Double
+Yearlychange = 0
 
-Totstockvol = 0 
+Dim Opening_val As Double
 
+Dim Closing_val As Double
+
+Dim Percentchange As Double
+
+Dim ToTvol As LongLong
+Totvol = 0
 
 
 'Summary table to keep track of tickers
@@ -27,58 +32,59 @@ Dim Summary_Table_Row As Integer
 
 'Loop for all spreadsheets
 
+For Each ws In Worksheets
 
-For Each WS In Worksheets
 
-'Name headers in sheet ( with a little bit of formatting)
 
-WS.Cells(1, 9).Value = "Ticker"
-WS.Cells(1, 10).Value = "Yearly Change"
-WS.Cells(1, 11).Value = "Percent Change"
-WS.Cells(1, 12).Value = "Total Stock Volume"
-WS.Range("A1:L1").Columns.AutoFit
-WS.Columns("G", "B").Columns.AutoFit
-WS.Range("A1:L1").HorizontalAlignment = xlCenter
+'Name headers in spreadsheet ( with a little bit of formatting)
 
-'Create last row variable
-lastrow = WS.Cells(Rows.Count, 1).End(xlUp).Row
-'Make loop
-For i = 2 To lastrow
+ws.Cells(1, 9).Value = "Ticker"
+ws.Cells(1, 10).Value = "Yearly Change"
+ws.Cells(1, 11).Value = "Percent Change"
+ws.Cells(1, 12).Value = "Total Stock Volume"
+ws.Range("A1:L1").Columns.AutoFit
+ws.Columns("G").Columns.AutoFit
+ws.Columns("B").Columns.AutoFit
+ws.Range("A1:L1").HorizontalAlignment = xlCenter
 
-'Conditional to find different tickers 
-If WS.Cells(i,1).Value <> WS.Cells(i+1, 1).Value Then 
+'Create lastrow variable
+ Dim lastrow as Long  
+  lastrow = Cells(Rows.Count, 1).End(xlUp).Row
+  
+  ' Loop through all the tickers 
+  For i = 2 To lastrow
 
-'locating values 
-ticker = WS.Cells(i,1).Value 
-Totstockvol = Totstockvol + WS.Cells(i,7).Value
+    ' If same ticker ..... if not 
+    If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
 
-'Print values 
-WS.Range("I" And Summary_Table_Row).Value = ticker 
-WS.Range("L" And Summary_Table_Row).Value = Totstockvol
+      ' Set ticker name
+      Ticker = Cells(i, 1).Value
 
-Summary_Table_Row = Summary_Table_Row + 1
+      ' Add to the Total Volume
+      TotVol = TotVol + Cells(i, 7).Value
 
-'Reset for next loop and ticker
-Totstockvol = 0 
+      ' Print ticker in summary table
+      Range("I" & Summary_Table_Row).Value = Ticker
 
-Else 
-Totstockvol = Totstockvol + WS.Cells(i,7).Value 
+      ' Print the total volume in table 
+      Range("L" & Summary_Table_Row).Value = TotVol
 
-End if 
+      ' Add one to the summary table row
+      Summary_Table_Row = Summary_Table_Row + 1
+      
+      ' Reset the total volume to prepare it for next ticker 
+      TotVol = 0
 
-Next i
+    ' If immediate cell is same ticker
+    Else
 
-Next WS
+      ' Add to the total volume
+      TotVol= TotVol + Cells(i, 7 ).Value
+
+    End If
+
+  Next i
+
+Next ws
 
 End Sub
-
-
-Sub bonus(): 
-
-'Make it work in all spreadsheets
-
-
-'Create variables 
-Dim Grt_increase as long 
-Dim Grt_decrease as long 
-Dim Grt_totvol as long 
